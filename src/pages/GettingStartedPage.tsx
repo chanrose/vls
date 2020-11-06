b059e3cbef2431368475e5c1d87b5f05cf483ae1
chanrose <bdchanbroset@gmail.com>
2020-11-06 23:05:58 +0700

    Updated getting started page

--- src/pages/GettingStartedPage.tsx
+++ src/pages/GettingStartedPage.tsx
@@ -1,27 +1,60 @@
-import React from 'react';
-import { IonAvatar, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './styles/GettingStartedPage.css';
+import React from "react";
+import {
+  IonAvatar,
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+  IonCol,
+  IonContent,
+  IonGrid,
+  IonHeader,
+  IonLabel,
+  IonPage,
+  IonRow,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import ExploreContainer from "../components/ExploreContainer";
+import "./styles/GettingStartedPage.css";
 
 const GettingStartedPage: React.FC = () => {
   return (
     <IonPage>
       <IonContent fullscreen>
-      <img src="../media/vlsIcon.png" />
-          <IonCard>
-            <IonAvatar><img src="../media/vlsIcon.png" /></IonAvatar>
-            <IonCardHeader>
-             <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
-              <IonCardTitle>Card Title</IonCardTitle>
-             </IonCardHeader>
-            
-           
-              <IonCardContent>
-                
-              </IonCardContent>
+        <IonCard className="ionCardstyle">
+          <IonCardHeader>
+            <img
+              className="imageSize"
+              src="https://raw.githubusercontent.com/chanrose/vls/v1.0/src/pages/media/vlsIcon.png"
+            />
+            <IonCardTitle className="centerText">Getting started</IonCardTitle>
+            <IonCardSubtitle className="centerText">
+              Choose language
+            </IonCardSubtitle>
+          </IonCardHeader>
 
-          </IonCard>
-        <ExploreContainer name="Tab 1 page" />
+          <IonCardContent>
+            <IonGrid>
+              <IonRow className="centerText">
+                <IonCol>
+                  <img src="https://cdn4.iconfinder.com/data/icons/europe-country-flags/105/UNITED_KINGDOM-512.png" />
+                  <br />
+                  <IonLabel>Thai</IonLabel>
+                </IonCol>
+                <IonCol>
+                  <div className="grayStraightLine"/>
+                  </IonCol>
+                <IonCol>
+                  <img src="https://cdn4.iconfinder.com/data/icons/europe-country-flags/105/UNITED_KINGDOM-512.png" />
+                  <br />
+                  <IonLabel>English</IonLabel>
+                </IonCol>
+              </IonRow>
+            </IonGrid>
+          </IonCardContent>
+        </IonCard>
       </IonContent>
     </IonPage>
   );
