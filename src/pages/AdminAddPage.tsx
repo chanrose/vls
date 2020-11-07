18fec5f92df391156c4d48e75674a8e238d2a499
chanrose <bdchanbroset@gmail.com>
2020-11-07 11:04:54 +0700

    Updated the Add new Page

--- src/pages/AdminAddPage.tsx
+++ src/pages/AdminAddPage.tsx
@@ -20,18 +20,22 @@ import {
   IonSegment,
   IonSegmentButton,
   IonText,
+  IonTextarea,
   IonTitle,
   IonToolbar,
 } from "@ionic/react";
 import ExploreContainer from "../components/ExploreContainer";
 import "./styles/GettingStartedPage.css";
+import "./styles/admin.css";
 
 const AdminAddPage: React.FC = () => {
   return (
     <IonPage>
-      <IonContent fullscreen>
-        
-        <IonSegment onIonChange={e => console.log('Segment selected', e.detail.value)}>
+      <IonHeader>
+        <IonToolbar>
+          <IonSegment
+            onIonChange={(e) => console.log("Segment selected", e.detail.value)}
+          >
             <IonSegmentButton value="Vehicle">
               <IonLabel>Vehicle</IonLabel>
             </IonSegmentButton>
@@ -42,11 +46,40 @@ const AdminAddPage: React.FC = () => {
               <IonLabel>Ticket</IonLabel>
             </IonSegmentButton>
           </IonSegment>
-          <IonGrid>
-            <IonRow>
-              <IonCol></IonCol>
-            </IonRow>
-          </IonGrid>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonList>
+          <IonItem>
+          <IonText>Sticker No. 1</IonText>
+          </IonItem>
+          <IonItem>
+           <IonInput type="text" placeholder="Vehicle Model" />
+          </IonItem>
+          <IonItem>
+           <IonInput type="text" placeholder="Tax status" />
+          </IonItem>
+          <IonItem>
+           <IonInput type="text" placeholder="Has greenbook?" />
+          </IonItem>
+          <IonItem>
+           <IonInput type="text" placeholder="Tax expired" />
+          </IonItem>
+          <IonItem>
+           <IonInput type="text" placeholder="Greenbook owner" />
+          </IonItem>
+          <IonItem>
+           <IonInput type="text" placeholder="Current Owner" />
+          </IonItem>
+          <IonItem>
+            <IonTextarea placeholder="Remark" />
+          </IonItem>
+          <IonRow>
+            <IonCol><IonButton fill="clear">Cancel</IonButton></IonCol>
+            <IonCol><IonButton className="floatRight" fill="clear">Add</IonButton></IonCol>
+          </IonRow>
+        </IonList>
+          
       </IonContent>
     </IonPage>
   );
