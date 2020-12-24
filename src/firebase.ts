837c40694fb85965e141c2636e6a6b03d8c96f55
Blubu <bdchanbroset@gmail.com>
2020-12-24 19:20:07 +0700

    update firebaes

--- src/firebase.ts
+++ src/firebase.ts
@@ -5,25 +5,14 @@ import 'firebase/storage';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAlZnO_WtOoBRPpzP93ApjMvqzybBKxR4c",
-    authDomain: "vehicle-legal-system.firebaseapp.com",
-    databaseURL: "https://vehicle-legal-system.firebaseio.com",
-    projectId: "vehicle-legal-system",
-    storageBucket: "vehicle-legal-system.appspot.com",
-    messagingSenderId: "750975524663",
-    appId: "1:750975524663:web:3d447f8e4f1a8ede4284b2",
-    measurementId: "G-TG7JM3L3QJ"
-  };
-
- /*  const firebaseConfig = {
-    apiKey: "AIzaSyCUXaQn64u7nPYO3_LEfTvjU0L76z0DjQ8",
-    authDomain: "vls-test-19edf.firebaseapp.com",
-    projectId: "vls-test-19edf",
-    storageBucket: "vls-test-19edf.appspot.com",
-    messagingSenderId: "286001692015",
-    appId: "1:286001692015:web:4ae3a7f45f9f5b7724fc9a",
-    measurementId: "G-TL4326YL7X"
-  }; */
+  apiKey: "AIzaSyCHNzPtpiIfSYjknX7h2fqncUoT37983PI",
+  authDomain: "vehicle-legal-system-56519.firebaseapp.com",
+  projectId: "vehicle-legal-system-56519",
+  storageBucket: "vehicle-legal-system-56519.appspot.com",
+  messagingSenderId: "992147345628",
+  appId: "1:992147345628:web:81e0a576366989aa21a419",
+  measurementId: "G-Y9QVSFNB8Q"
+};
 
   
 const app = firebase.initializeApp(firebaseConfig);
